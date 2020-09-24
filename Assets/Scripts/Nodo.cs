25110693afe385d554186e25b56767e928c0d34f
Chorri <josemariacosta_8@hotmail.com>
2020-09-24 02:59:00 -0500

    Agregado proyecto de unity con A*

---
+++ Assets/Scripts/Nodo.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nodo : MonoBehaviour
+{
+
+    public int trabajo = 0;
+    public TextMesh trabajoTexto;
+    public int distancia = 0;
+    public TextMesh distanciaTexto;
+    public Nodo direccion;
+
+    public List<Nodo> adjacentes;
+
+    //Valores Unity
+    Transform objeto;
+    List<BoxCollider> busquedaAdjacentes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        objeto = this.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (var i in adjacentes) {
+            Debug.DrawLine(objeto.position,i.GetTransform().position,Color.red);
+        }
+        trabajoTexto.text = trabajo.ToString();
+        distanciaTexto.text = distancia.ToString();
+    }
+
+    public int CostoTotal() {
+        return trabajo + distancia;
+    }
+
+    public Transform GetTransform() {
+        return objeto;
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag != "Finish" && other.transform.parent != this.transform) {
+            adjacentes.Add(other.transform.parent.GetComponent<Nodo>());
+        }
+        
+    }
+}
---
+++ Assets/Scripts/Pathfinding.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinding : MonoBehaviour
+{
+
+    public Nodo inicio;
+    public Nodo final;
+    
+    // Start is called before the first frame update
+    void Update()
+    {
+        AStar(inicio, final);
+    }
+
+    void AStar(Nodo ini,Nodo fin) {
+
+        List<Nodo> posibles = new List<Nodo>();
+        List<Nodo> revisados = new List<Nodo>();
+        posibles.Add(ini);
+
+        //Mientras no se explore el fin
+        while (!revisados.Contains(fin)) {
+            //Definir cual es el nodo actual entre todos los nodos posibles
+            Nodo actual = posibles[0];
+            for (int i = 0; i < posibles.Count; i++) {
+                if (posibles[i].CostoTotal() < actual.CostoTotal() ||
+                    posibles[i].CostoTotal() == actual.CostoTotal() && posibles[i].distancia<actual.distancia) {
+                    actual = posibles[i];
+                }
+            }
+
+            //Remover y agregar a la lista de revisados el actual
+            posibles.Remove(actual);
+            revisados.Add(actual);
+
+            if (actual==fin) {
+                MostrarCamino(inicio, final);
+                return;
+            }
+
+            foreach (var item in actual.adjacentes) {
+                if (revisados.Contains(item)) {
+                    continue;
+                }
+
+                int nuevoTrabajo = actual.trabajo + CalcDistancia(actual, item);
+                if (nuevoTrabajo < item.trabajo || !posibles.Contains(item)) {
+                    item.trabajo = nuevoTrabajo;
+                    item.distancia = CalcDistancia(item, fin);
+                    item.direccion = actual;
+
+                    if (!posibles.Contains(item)) {
+                        posibles.Add(item);
+                    }
+                }
+            }
+        }
+    }
+
+    void MostrarCamino(Nodo i, Nodo f) {
+        List<Nodo> camino = new List<Nodo>();
+        Nodo actual = f;
+
+        while (actual != i) {
+            camino.Add(actual);
+            actual.GetTransform().GetChild(0).gameObject.SetActive(false);
+            actual = actual.direccion;
+        }
+        camino.Add(actual);
+        actual.GetTransform().GetChild(0).gameObject.SetActive(false);
+    }
+
+    int CalcDistancia(Nodo a,Nodo b) {
+        return (int) Vector3.Distance(a.GetTransform().position, b.GetTransform().position);
+    }
+
+}
