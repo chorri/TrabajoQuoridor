3dd9ad611aa326108c5caeb28e745f808a8c5dd2
Chorri <josemariacosta_8@hotmail.com>
2021-04-25 09:58:01 -0500

    Moved scripts to a Game folder and added GameManager

---
+++ Assets/Scripts/Game/Game_Manager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public enum GameState
+{
+    Start,
+    Run,
+    Result
+}
+
+public class Game_Manager : MonoBehaviour
+{
+    public static Game_Manager instance;
+
+    public GameState currentGameState;
+    public GameObject lightObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ResetLevel when pressing O
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
+        switch (currentGameState)
+        {
+            case GameState.Start:
+                lightObject.SetActive(false);
+                break;
+            case GameState.Run:
+                lightObject.SetActive(true);
+                break;
+            case GameState.Result:
+                lightObject.SetActive(false);
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/Game/PlayerGoalPlacer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGoalPlacer : MonoBehaviour
+{
+    public Transform player;
+    public Transform goal;
+    Transform parent;
+    public Vector3 offset;
+    public Material start;
+    public Material end;
+
+    void Start()
+    {
+        parent = transform.parent;
+    }
+
+    private void OnMouseOver()
+    {
+        Debug.Log("Test");
+        if (Game_Manager.instance.currentGameState == GameState.Start)
+        {
+            //Place Player
+            if (Input.GetKey(KeyCode.Mouse0))
+            {
+                player.position = parent.position + offset;
+                GetComponent<MeshRenderer>().material = start;
+            }
+
+            //Place Goal
+            if (Input.GetKey(KeyCode.Mouse1))
+            {
+                goal.position = goal.position + offset;
+                GetComponent<MeshRenderer>().material = end;
+            }
+        }
+    }
+}
